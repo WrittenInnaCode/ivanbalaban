fafacdced0dc14f61e347da1ec94d12b74f2d8cd
WrittenInnaCode <writteninnacode@gmail.com>
2024-07-30 22:58:27 -0700

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,9 @@ const About = () => {
                 <div id="ivan-information">
                     <p id="personal-stylist">Your personal hairstylist</p>
                     <p id="phone-number">(818) 674-3491</p>
-                    <p id="about-paragraph">Here you will write a little bit about yourself.</p>
+                    <p id="about-paragraph">Hello! My name is Ivan, and I am a professional hairdresser with over 10 years of experience. 
+                    I specialize in creating personalized looks tailored to each of my clients, whether you're seeking a timeless classic cut 
+                    or the latest trend. My goal is to ensure you leave feeling fantastic and loving your new hair!</p>
                 </div>
 
                 <div id="ivan-photo-box">
